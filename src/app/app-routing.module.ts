b9c52bcac9727aaeb4aab8ea5a581cfab636e6b4
iv.zagoruiko <iv.zagoruiko@gmail.com>
2020-03-11 22:54:31 +0200

    import video from youtube

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', component: HomePageComponent, pathMatch: 'full'}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
