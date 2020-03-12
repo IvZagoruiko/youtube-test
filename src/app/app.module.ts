71bbd7dc5584c5360494584f6fc373092844cf44
iv.zagoruiko <iv.zagoruiko@gmail.com>
2020-03-12 14:10:50 +0200

    add angular-youtube-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PlayerComponent } from './player/player.component';
+import {YouTubePlayerModule} from '@angular/youtube-player';
+import { AngularPlayerComponent } from './angular-player/angular-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
     HomePageComponent,
-    PlayerComponent
+    PlayerComponent,
+    AngularPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    YouTubePlayerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
